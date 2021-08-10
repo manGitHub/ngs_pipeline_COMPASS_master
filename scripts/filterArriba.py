5e21de8d86cbf5fbc6ef68616c90f22ec25cd285
kvaldez <valdezkm@biowulf.nih.gov>
2021-08-10 13:30:51 -0400

    Update arriba filter

--- scripts/filterArriba.py
+++ scripts/filterArriba.py
@@ -5,12 +5,12 @@ inFile = sys.argv[1]
 outFile = sys.argv[2]
 
 # create a column that appends 3 columns of read counts
-# filter out fusions with more than 1 zero and low confidence
+# filter out fusions with more than 1 zero 
 tbl = pd.read_csv(inFile,delimiter='\t')
 tbl['allThree'] = tbl[['split_reads1','split_reads2','discordant_mates']].apply(
-    lambda x: ' '.join(x.dropna().astype(str)),
+    lambda x: x.dropna().tolist(),
     axis=1
 )
-tbl['countZeros'] = tbl.allThree.str.count('0')
-tbl = tbl.loc[(tbl.countZeros<2) | (tbl.confidence != 'low')]
+tbl['countZeros'] = [x.count(0) for x in tbl['allThree']]
+tbl = tbl.loc[tbl.countZeros<2]
 tbl.to_csv(outFile,index=False,sep='\t')

5cc1bd7659a4931f98af3364e529e6a83fe5a3de
kvaldez <valdezkm@biowulf.nih.gov>
2024-03-20 17:18:59 -0400

    fix bug in Arriba fusion filter

--- scripts/filterArriba.py
+++ scripts/filterArriba.py
@@ -8,21 +8,30 @@ starFusFile = sys.argv[4]
 
 #breakpoints in fusion catcher and star fusion
 fuseCat = pd.read_csv(fuseCatFile,delimiter='\t')
-fuseCat = fuseCat["Fusion_point_for_gene_1(5end_fusion_partner)"].values.tolist() + fuseCat["Fusion_point_for_gene_2(3end_fusion_partner)"].values.tolist()
-fuseCat = ["chr" + ":".join(x.split(':')[:-1]) for x in fuseCat]
+fuseCat['Fusion_point_for_gene_1(5end_fusion_partner)'] = ['chr' + ':'.join(x) for x in fuseCat["Fusion_point_for_gene_1(5end_fusion_partner)"].str.split(':').str[:-1]]
+fuseCat['Fusion_point_for_gene_2(3end_fusion_partner)'] = ['chr' + ':'.join(x) for x in fuseCat["Fusion_point_for_gene_2(3end_fusion_partner)"].str.split(':').str[:-1]]
+fuseCat['fusions'] = fuseCat['Fusion_point_for_gene_1(5end_fusion_partner)'] + '-' + fuseCat["Fusion_point_for_gene_2(3end_fusion_partner)"]
+fuseCat['reverseFusions'] = fuseCat["Fusion_point_for_gene_2(3end_fusion_partner)"] + '-' + fuseCat['Fusion_point_for_gene_1(5end_fusion_partner)']
+fuseCat = fuseCat['fusions'].values.tolist() + fuseCat['reverseFusions'].values.tolist()
+
 starFus = pd.read_csv(starFusFile,delimiter='\t')
-starFus = starFus["RightBreakpoint"].values.tolist() + starFus["LeftBreakpoint"].values.tolist()
-starFus = [":".join(x.split(':')[:-1]) for x in starFus]
-genes = set(fuseCat + starFus)
+starFus['RightBreakpoint'] = [':'.join(x) for x in starFus["RightBreakpoint"].str.split(':').str[:-1]]
+starFus['LeftBreakpoint'] = [':'.join(x) for x in starFus["LeftBreakpoint"].str.split(':').str[:-1]]
+starFus['fusions'] = starFus['RightBreakpoint'] + '-' + starFus['LeftBreakpoint']
+starFus['reverseFusions'] = starFus['LeftBreakpoint'] + '-' + starFus['RightBreakpoint']
+starFus = starFus['fusions'].values.tolist() + starFus['reverseFusions'].values.tolist()
+
+fuss = set(fuseCat + starFus)
 
 # create a column that appends 3 columns of read counts
 # filter out fusions with more than 1 zero 
-# keep if contains breakpoint in either STARfusion or fusionCatcher
+# keep if contains fusion in either STARfusion or fusionCatcher
 tbl = pd.read_csv(inFile,delimiter='\t')
 tbl['allThree'] = tbl[['split_reads1','split_reads2','discordant_mates']].apply(
     lambda x: x.dropna().tolist(),
     axis=1
 )
 tbl['countZeros'] = [x.count(0) for x in tbl['allThree']]
-tbl = tbl.loc[(tbl.countZeros<2) | (tbl['breakpoint1'].isin(genes)) | (tbl['breakpoint2'].isin(genes))]
+tbl['fusions'] = tbl['breakpoint1'] + '-' + tbl['breakpoint2']
+tbl = tbl.loc[(tbl.countZeros<2) | (tbl['fusions'].isin(fuss))]
 tbl.to_csv(outFile,index=False,sep='\t')
